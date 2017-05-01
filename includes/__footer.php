c25e555d6e1e01ad08bac0236bff613c0bf683d5
dharif23 <dalwar014@gmail.com>
2017-05-01 17:38:48 +0200

    Added presentaiton download

--- includes/__footer.php
+++ includes/__footer.php
@@ -12,12 +12,15 @@ echo "
   All rights reserved &copy; 2016 - $year <a href='http://www.inet.tu-berlin.de' target='_blank'>www.inet.tu-berlin.de</a>&nbsp; ||
 ";
 echo "
-  <strong>Webportal developed by :</strong>&nbsp;<a href='http://www.user.tu-berlin.de/hossainarif' target='_blank'>Dalwar Hossain</a>&nbsp; ||
+  <strong>Webportal developed by:</strong>&nbsp;<a href='http://www.user.tu-berlin.de/hossainarif' target='_blank'>Dalwar Hossain</a>&nbsp; ||
 ";
 echo "
   <strong>Project group:</strong>&nbsp;<a href='http://www.user.tu-berlin.de/hossainarif' target='_blank'>Dalwar Hossain</a>, <a href='https://www.linkedin.com/in/omar-faruque-3066b324/' target='_blank'>Omar Faruque</a>, <a href='https://www.linkedin.com/in/shihabkarim/​' target='_blank'>Shihab Karim</a>, <a href='https://www.linkedin.com/in/kazi-m-hossain-40000633/' target='_blank'>Kazi Hossain</a> &nbsp; ||
 ";
 echo "
-  <strong>Project instructor:</strong>&nbsp;<a href='http://www.inet.tu-berlin.de/menue/people/research_assistants/tkrenc/' target='_blank'>Thomas Krenc</a>
+  <strong>Project instructor:</strong>&nbsp;<a href='http://www.inet.tu-berlin.de/menue/people/research_assistants/tkrenc/' target='_blank'>Thomas Krenc</a>&nbsp; ||
+";
+echo "
+  <strong>Presentation:</strong>&nbsp;<a href='presentation/Prefix_Delegation.pdf' target='_blank'>Download</a>
 ";
 ?>
